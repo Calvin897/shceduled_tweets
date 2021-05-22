c6d53b63797745c3e65e3d1dc0cf5d37159aa53a
calvin tribelhorn <calvintribby@gmail.com>
2021-05-22 13:38:45 +0200

    final commit, made changes to date time

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,30 @@ Rails.application.routes.draw do
   #Get /about
   get "about", to: "about#index"
 
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  
+  get "sign_in", to: "session#new"
+  post "sign_in", to: "session#create"
+  
+  delete "logout", to: "session#destroy"
+
+  get "password/reset", to: "password_reset#new"
+  post "password/reset", to: "password_reset#create"
+
+  get "password/reset/edit", to: "password_reset#edit"
+  patch "password/reset/edit", to: "password_reset#update"
+
+  get "auth/twitter/callback", to: "omniauth_callbacks#twitter"
+
+  resources :twitter_accounts
+  resources :tweets
+
+
   root to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
